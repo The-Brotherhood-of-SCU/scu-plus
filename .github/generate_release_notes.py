bbd0375a6295ac90e2e7d9d649a98d9964d90b26
57U <ming57u@foxmail.com>
2025-06-09 21:16:42 +0800

    update github actions

---
+++ .github/generate_release_notes.py
@@ -0,0 +1,58 @@
+import os
+import subprocess
+import re
+
+duplicated=[]
+
+def filter(commit:str)->bool:
+    lower=commit.lower()
+    if lower in duplicated:
+        return True
+    duplicated.append(lower)
+    pattern="updates? \(.+?\)"
+    if re.search(pattern,lower):
+        return True
+    return False
+
+powershell_path = r"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+
+def get_release_notes():
+    repo_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # 获取上一个tag
+    try:
+        prev_tag = subprocess.check_output(
+            "git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1)",
+            shell=True, 
+            text=True,
+            cwd=repo_root,
+            executable=powershell_path # cmd 会报错
+        ).strip()
+    except subprocess.CalledProcessError as e:
+        prev_tag = None
+    
+    print(f"prev_tag: {prev_tag}")
+
+    # 根据是否有上一个tag获取commit日志
+    if not prev_tag:
+        notes = subprocess.check_output(
+            "git log --pretty=format:\"- %s (%an)\" --no-merges",
+            shell=True, text=True
+        )
+    else:
+        notes = subprocess.check_output(
+            f"git log {prev_tag}..HEAD --pretty=format:\"- %s (%an)\" --no-merges",
+            shell=True, text=True
+        )
+    #filter "update"
+    notes = "\n".join([note for note in notes.split("\n") if not filter(note)])
+    
+    return f"### 主要更新\n\n{notes}"
+
+if __name__ == "__main__":
+    release_notes = get_release_notes()
+    print(release_notes)
+    if "GITHUB_ENV" in os.environ:
+        with open(os.environ["GITHUB_ENV"], "a") as f:
+            f.write(f"release_notes<<EOF\n{release_notes}\nEOF\n")
+    else:
+        print("GITHUB_ENV not found. Release notes not exported.")
