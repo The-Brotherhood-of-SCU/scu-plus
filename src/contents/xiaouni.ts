c6dbe53c839e8fb57416bea23b3b2a4be9c85f44
jeanhua <2207739460@qq.com>
2026-04-03 21:30:08 +0800

    refactor(market): 移除废弃的校园集市增强

--- src/contents/xiaouni.ts
+++
@@ -1,9 +0,0 @@
-import type { PlasmoCSConfig } from "plasmo";
-import { initMarketEnhance } from "~features/market-enhance";
-
-export const config: PlasmoCSConfig = {
-    matches: ["https://u.xiaouni.com/mobile/pages/pd/*"],
-    all_frames: true,
-};
-
-initMarketEnhance();
